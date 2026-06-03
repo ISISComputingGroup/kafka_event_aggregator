d4b95a9cb6cc81d16954be2caa38dcb716c1bfe7
rerpha <rerpha@users.noreply.github.com>
2026-06-03 09:17:51 +0000

    deploy: 6104e3b09218936d6f9445b0877c2f7a9ba25c63

---
+++ trait.impl/serde_core/de/trait.Deserialize.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["kafka_event_aggregator",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.Deserialize.html\" title=\"trait serde_core::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"kafka_event_aggregator/config/struct.AggregatorConfig.html\" title=\"struct kafka_event_aggregator::config::AggregatorConfig\">AggregatorConfig</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[400]}
